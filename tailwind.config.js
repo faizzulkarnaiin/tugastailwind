4bb3eb2c8f8a5ce9b7042013d68ca113ac1f447b
faizzulkarnaiin <faizmaqusa7@gmail.com>
2023-03-11 14:03:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'coder': "url('../img/Rectangle 1.png')"
+        
+      }
+    },
+  },
+  plugins: [],
+}
